aad3c39bab661a1c34c60028f1f6c3babead2c94
GeryTrstno <gerytauristino8@gmail.com>
2025-05-12 22:04:42 +0700

    Update Profile Setting

--- resources/views/partials/settings-heading.blade.php
+++ resources/views/partials/settings-heading.blade.php
@@ -1,5 +1,20 @@
-<div class="relative mb-6 w-full">
-    <flux:heading size="xl" level="1">{{ __('Settings') }}</flux:heading>
-    <flux:subheading size="lg" class="mb-6">{{ __('Manage your profile and account settings') }}</flux:subheading>
-    <flux:separator variant="subtle" />
+<div class="flex items-start justify-between">
+
+    <div class="relative mb-6 w-full">
+        <flux:heading size="xl" level="1">{{ __('Settings') }}</flux:heading>
+        <flux:subheading size="lg" class="mb-6">{{ __('Manage your profile and account settings') }}</flux:subheading>
+    </div>
+
+    <div class="ml-4 flex-shrink-0">
+        <form action="{{ route('logout') }}" method="post">
+            @csrf
+            <flux:button type="submit" size="sm" class="w-[10vh] mt-2" variant="primary">
+                {{ __('Log out') }}
+            </flux:button>
+        </form>
+
+    </div>
+
 </div>
+
+<flux:separator class="mb-5"></flux:separator>
