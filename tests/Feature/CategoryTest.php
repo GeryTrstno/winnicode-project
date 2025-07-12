bc38ed82c74a216e3336354784e607d388678932
GeryTrstno <gerytauristino8@gmail.com>
2025-07-12 17:58:44 +0700

    Fix Test

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -5,7 +5,7 @@ use App\Models\User;
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
 test('guests are redirected to the login page when accessing categories', function () {
-    $response = $this->get('/categories'); // Mengakses halaman categories sebagai tamu
+    $response = $this->get('/categories/economy'); // Mengakses halaman categories sebagai tamu
     $response->assertRedirect('/login');
 });
 
@@ -13,7 +13,7 @@ test('authenticated users can visit categories', function () {
     $user = User::factory()->create();
     $this->actingAs($user);
 
-    $response = $this->get(route('categories')); // Mengakses halaman categories
+    $response = $this->get(route('categories/economy')); // Mengakses halaman categories
     $response->assertStatus(200); // Memastikan status code 200 (OK)
 });
 
